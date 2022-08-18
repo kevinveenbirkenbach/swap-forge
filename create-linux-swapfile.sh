d68cc2c18ea10f6c494bbd6a425f51d99f30a601
Kevin Veen-Birkenbach <kevin@veen.world>
2022-08-18 18:00:44 +0200

    Update create-linux-swapfile.sh
    Added size parameter

--- create-linux-swapfile.sh
+++ create-linux-swapfile.sh
@@ -6,9 +6,9 @@ if grep -q "$FSTAB_SWAP_ENTRY" "$FSTAB_FILE"; then
 	echo "Skipping creation of swap partion because entry allready exists in \"$FSTAB_FILE\"!"
 else
 	echo "Creating swap partition..." &&
-	sudo fallocate -l 16G "$SWAP_FILE" &&
+	sudo fallocate -l "$1" "$SWAP_FILE" &&
 	sudo chmod 600 "$SWAP_FILE" &&
 	sudo mkswap "$SWAP_FILE" &&
 	sudo swapon "$SWAP_FILE" &&
-	sudo sh -c "echo \"$FSTAB_SWAP_ENTRY\">>\"$FSTAB_FILE\""
+	sudo sh -c "echo \"$FSTAB_SWAP_ENTRY\">>\"$FSTAB_FILE\"" || exit 1
 fi
