2e2c4b4711612fda33b8c4b7b36a269248226670
Kevin Veen-Birkenbach <kevin@veen.world>
2022-08-18 17:31:26 +0200

    Create create-linux-swap-file.sh

---
+++ create-linux-swap-file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FSTAB_SWAP_ENTRY="/swapfile none swap defaults 0 0"
+SWAP_FILE="/swapfile"
+FSTAB_FILE="/etc/fstab"
+if grep -q "$FSTAB_SWAP_ENTRY" "$FSTAB_FILE"; then
+	echo "Skipping creation of swap partion because entry allready exists in \"$FSTAB_FILE\"!"
+else
+	echo "Creating swap partition..." &&
+	sudo fallocate -l 16G "$SWAP_FILE" &&
+	sudo chmod 600 "$SWAP_FILE" &&
+	sudo mkswap "$SWAP_FILE" &&
+	sudo swapon "$SWAP_FILE" &&
+	sudo sh -c "echo \"$FSTAB_SWAP_ENTRY\">>\"$FSTAB_FILE\""
+fi
